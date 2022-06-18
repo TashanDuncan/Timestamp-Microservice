25f8c57e88b7cd148cc0fdeaee84563ed404aef4
viewtifult <tashanduncan1994@gmail.com>
2022-06-18 14:10:16 +0100

    complete challenge, tests complete

--- index.js
+++ index.js
@@ -6,40 +6,46 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
 //
-app.get("/api/:date", function (req, res) {
-  const {date: date} = req.params;
-  
-  if ((/\d{4}-\d{2}-\d{2}/).test(date)) { // if ####-##-##
-    const unix = Math.floor(new Date(date).getTime() / 1000)
-    const fullDate = new Date(date);
-    return res.json({unix: unix,utc:fullDate.toUTCString()});
-  } 
-   else {
-    return res.json({ error : "Invalid Date" });
-  }
-
-  
+app.get('/api', function (req, res) {
+  const now = new Date();
+  const unix = now.getTime();
+  return res.json({ unix: unix, utc: now.toUTCString() });
 });
 
+app.get('/api/:date', function (req, res) {
+  const { date: date } = req.params;
+  const parsedDate = Date.parse(date);
+  if (/^[0-9]{13,13}$/.test(date)) {
+    const unix = +date;
+    const fullDate = new Date(unix);
+    return res.json({ unix: unix, utc: fullDate.toUTCString() });
+  } else if (isNaN(parsedDate)) {
+    return res.json({ error: 'Invalid Date' });
+  } else {
+    const fullDate = new Date(date);
+    const unix = parsedDate;
+
+    return res.json({ unix: unix, utc: fullDate.toUTCString() });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(5000, function () {
